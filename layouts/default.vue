e1525f09de6b634d93701484e8d0486e26f18335
varunumesh <varun@zysk.in>
2021-01-18 12:18:46 +0530

    Deafult template setup
    Because:
    - Added a header with logo and few icons
    - Functionality for the icons is WIP
    - Added a footer with backlinks to the main site
    - Styles for the footer is WIP
    - Links for the footer is WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,72 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <v-app>
+    <v-app-bar
+      color="white"
+      elevate-on-scroll
+      height="96"
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-toolbar-title>
+        <img
+          class="logo px-4 mt-4"
+          src="@/assets/images/ZyskLogo.svg"
+          alt="zysk-logo"
+        />
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-star</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-information</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <Nuxt id="scrolling-techniques-7" />
+
+    <v-footer color="secondary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="secondary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Zysk Technologies</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      links: ['Home', 'Services', 'Contact Us'],
+    }
+  },
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+body {
+  border: 0.5rem solid #ff4b4b;
+  border-bottom: 0;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.logo {
+  max-width: 100%;
 }
 </style>
