d6f18b11e62b1a50a6074c6ecb6360d8b28fa02c
varunumesh <varun@zysk.in>
2021-01-18 12:22:04 +0530

    Meetings rooms landing page
    Because:
    - Home page fetcheds data from rooms.json file to show MeetingRooms
    - MeetingRooms component displays all the meetings rooms
    - Limit added to the Nuxt/content query builder to show only 8 rooms

---
+++ components/MeetingRooms.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-col v-for="room in rooms" :key="room.name" cols="6" sm="4" md="4" lg="3">
+      <v-card hover ripple :href="room.googleMeet" target="_blank">
+        <v-img
+          :src="room.profileImage"
+          class="white--text align-end"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          height="160px"
+        >
+          <v-card-title
+            class="text-subtitle-2"
+            v-text="room.name"
+          ></v-card-title>
+        </v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    rooms: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,73 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">meeting-rooms</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" lg="8">
+        <h1 class="text-h3 font-weight-thin text-center mt-16 mb-8">
+          Let's get talking...
+        </h1>
+
+        <v-text-field
+          v-model="query"
+          type="search"
+          autocomplete="off"
+          placeholder="Who do you want to talk to?"
+          prepend-inner-icon="mdi-magnify"
+          class="search-bar"
+          single-line
+          filled
+          rounded
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12" md="6">
+        <h2 class="text-h5 font-weight-thin my-4">Frequently accessed</h2>
+        <MeetingRooms :rooms="rooms" class="my-6 meeting-rooms" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
+import MeetingRooms from '~/components/MeetingRooms.vue'
+export default {
+  components: { MeetingRooms },
+
+  async asyncData({ $content }) {
+    const rooms = await $content()
+      .sortBy('frequentlyUsed', 'desc')
+      .sortBy('name')
+      .limit(8)
+      .fetch()
+
+    return { rooms }
+  },
+
+  data() {
+    return {
+      query: '',
+    }
+  },
+
+  watch: {
+    async query() {
+      this.rooms = await this.$content()
+        .sortBy('frequentlyUsed', 'desc')
+        .sortBy('name')
+        .limit(8)
+        .search(this.query)
+        .fetch()
+    },
+  },
+}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
+<style lang="scss" scoped>
+.search-bar {
+  max-width: 584px;
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.meeting-rooms {
+  min-height: 368px;
 }
 </style>
