11a8f1f122c16cd3ee34a81e1e84db225f41637c
Supritha R S <suprithars111@gmail.com>
2021-07-20 16:33:59 +0530

    search fucntion and pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,17 +7,6 @@ use Inertia\Inertia;
 
 class HomeController extends Controller
 {
-    // public function index(){
-        // return Inertia::render('home/index');
-
-    // }
-
-    // public function index(User $user)
-    // {
-    //     $user = User::paginate(10);
-    //     return $user;
-    // }
-
     public function index(User $users)
     {
         $users = User::orderBy('id', 'desc')
